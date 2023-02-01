8a4fb3c44692cb21a8e6b978da5f6da02a200304
Pablo Montañana Fuentes <pablomontanana@gmail.com>
2023-02-01 20:51:01 +0100

    Finalizacion ejercicio Pop-Up

--- JavaScript/Pop-Up/popup.js
+++ JavaScript/Pop-Up/popup.js
@@ -1,10 +1,24 @@
-var edad;
+function ventana(mensaje, fondo, imagen, titulo){
+        nuevaventana=window.open("" ,  "" ,"width=350,height=350");
+        nuevaventana.document.write(mensaje);
+        nuevaventana.document.bgColor=fondo;
+        nuevaventana.document.write("<img src='"+ imagen + "' style='height: 5cm; width: 8cm;'>");
+        nuevaventana.document.title=(titulo);
+    }
 
 function boton() {
-    switch(edad){
-        case 1;
+    let edad=document.getElementById("edad").value;
 
-
-            break;
+    if(edad<0){
+        //No ejecuta nada
+    }else if(edad<19){
+        ventana("Venta de monopatin negro", "red", "skate.png", "hola");
+    }else if(edad<41){
+        ventana("Gama suv de fiat con hasta un 50% de descuento", "aqua", "fiat.png" , "Adios");
+    }else if(edad<=100){
+        ventana("Hoteles al mejor precio en Benidorm", "green", "benidorm.jpg" ,"Depende");
+    }else if(edad>101){
+        //No ejecuta nada
     }
 }
+
